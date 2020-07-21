4889339151db7a0afbde9fb8ebebdcd27c2c9a21
denisgromada <denisgromada98@gmail.com>
2020-07-21 12:29:28 +0300

    Second point

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+" use strict ";
+
 const numberOfFilms = +prompt('How much movie are you watch alredy?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const   a = prompt ('One of the last movie witch you watch?', ''),
-        b = prompt ('What are you mark for it?', ''),
-        c = prompt ('One of the last movie witch you watch?', ''),
-        d = prompt ('What are you mark for it?', '');
+for (let i = 0; i < 2; i++) {
+    const   a = prompt ('One of the last movie witch you watch?', ''),
+            b = prompt ('What are you mark for it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a !=null && b !=null && a != '' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log ('Little');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ('Classic');
+}  else if (personalMovieDB.count >= 30 ) {
+    console.log ('Good');
+} else {
+    console.log('ERROR');
+}
 
 console.log(personalMovieDB);
